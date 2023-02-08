73080e0707d6ce3a4631c1fcdf12f9b8749b998e
jasmine tan <jtan25@cmc.edu>
2023-02-07 19:22:03 -0800

    html valid py flake8

--- HTML_Validator.py
+++ HTML_Validator.py
@@ -3,7 +3,8 @@
 
 def validate_html(html):
     '''
-    This function performs a limited version of html validation by checking whether every opening tag has a corresponding closing tag.
+    This function performs a limited version of
+    html validation by checking whether every opening tag has a corresponding closing tag.
 
     >>> validate_html('<strong>example</strong>')
     True
@@ -12,12 +13,12 @@ def validate_html(html):
     '''
 
     # HINT:
-    # use the _extract_tags function below to 
+    # use the _extract_tags function below to
     # generate a list of html tags without any extra text;
-    # then process these html tags using the 
+    # then process these html tags using the
     # balanced parentheses algorithm from the class/book
-    # the main difference between your code and the code from class 
-    # will be that you will have to 
+    # the main difference between your code and the code from class
+    # will be that you will have to
     # keep track of not just the 3 types of parentheses,
     # but arbitrary text located between the html tags
     tags = _extract_tags(html)
@@ -41,12 +42,15 @@ def validate_html(html):
         else:
             return False
 
+
 def _extract_tags(html):
     '''
     This is a helper function for `validate_html`.
-    By convention in Python, helper functions that are not meant to be used directly by the user are prefixed with an underscore.
-
-    This function returns a list of all the html tags contained in the input string,
+    By convention in Python, helper functions
+    that are not meant to be used directly by the
+    user are prefixed with an underscore.
+    This function returns a list of all
+    the html tags contained in the input string,
     stripping out all text not contained within angle brackets.
 
     >>> _extract_tags('Python <strong>rocks</strong>!')
