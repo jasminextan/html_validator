f9a5f61946c1038896ef3853477208110d33fc39
jasmine tan <jtan25@cmc.edu>
2023-02-07 19:13:55 -0800

    html valid py and readme fixed

--- HTML_Validator.py
+++ HTML_Validator.py
@@ -12,11 +12,34 @@ def validate_html(html):
     '''
 
     # HINT:
-    # use the _extract_tags function below to generate a list of html tags without any extra text;
-    # then process these html tags using the balanced parentheses algorithm from the class/book
-    # the main difference between your code and the code from class will be that you will have to keep track of not just the 3 types of parentheses,
+    # use the _extract_tags function below to 
+    # generate a list of html tags without any extra text;
+    # then process these html tags using the 
+    # balanced parentheses algorithm from the class/book
+    # the main difference between your code and the code from class 
+    # will be that you will have to 
+    # keep track of not just the 3 types of parentheses,
     # but arbitrary text located between the html tags
-
+    tags = _extract_tags(html)
+    if '<' in html and '>' not in html:
+        return False
+    else:
+        stack = []
+        for tag in tags:
+            if tag[1] != '/':
+                stack.append(tag)
+            else:
+                if len(stack) == 0:
+                    return False
+                else:
+                    if stack[-1][1:-1] == tag[2:-1]:
+                        stack.pop()
+                    else:
+                        return False
+        if len(stack) == 0:
+            return True
+        else:
+            return False
 
 def _extract_tags(html):
     '''
@@ -29,3 +52,22 @@ def _extract_tags(html):
     >>> _extract_tags('Python <strong>rocks</strong>!')
     ['<strong>', '</strong>']
     '''
+    inside_tag = False
+    tags = []
+    tag = ''
+    for char in html:
+        if char == '<':
+            inside_tag = True
+        if char == '>':
+            tag += char
+            inside_tag = False
+            tags.append(tag)
+            tag = ''
+        if inside_tag is True:
+            if char == ' ':
+                inside_tag = False
+            else:
+                tag += char
+        else:
+            pass
+    return tags
